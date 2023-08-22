edbabba79c9719113d7943422a3730cca6b955cf
YungChun Tu <ken888686@gmail.com>
2023-08-22 23:10:37 +0900

    initial commit

---
+++ src/main/java/com/aaron/springrunner/SpringRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.aaron.springrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aaron/springrunner/SpringRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaron.springrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
